00209475336d64129947d2574a8939762c7cc733
Daniel <ds@danielstegen.de>
2020-07-03 19:12:26 +0200

    students display in teachers view

--- teacher/views/view.js
+++ teacher/views/view.js
@@ -17,7 +17,7 @@ function teacherView (teacher) {
       <hr />
     </div>
     <div id="class" class="container my-3 p-3 border collapse show" data-parent="#homeschool-ds">
-      <h2>Klasse/n</h2>
+      <h2>Klasse/n von ${teacher.fname} ${teacher.lname}</h2>
       <hr />
       ${teacher.group.map(displayClass).join('')}
     </div>
@@ -28,23 +28,29 @@ function teacherView (teacher) {
   `;
 }
 
+
 // Additional functions
 
 function displayClass (group) {
   let returnHtml = `<h4 class="pt-3 pb-1 px-1">Klasse: ${group}</h4>`;
-  returnHtml += '<table class="table border"><tr><th>Nr.</th><th>Vorname</th><th>Nachname</th></tr>';
+  returnHtml += '<table class="table border"><tr><th>Nr.</th><th>Vorname</th><th>Nachname</th><th>Telefon</th></tr>';
   getAllUsers(group).filter( person => person.role === 'student').forEach((item, i) => {
     returnHtml += `
       <tr>
-        <td>${i}</td>
+        <td>${i+1}</td>
         <td>${item.fname}</td>
         <td>${item.lname}</td>
-        <td><button class="btn-sm btn-secondary">Edit</button></td>
-        <td><button class="btn-sm btn-primary" onclick="sendEmail(${item.email});">E-Mail</button></td>
+        <td>${item.phone}</td>
+        <td class="d-flex justify-content-end">
+          <button class="btn-sm btn-primary">Edit</button>
+          <button class="btn-sm btn-secondary ml-2" onclick="sendEmail('${item.email}');">E-Mail</button>
+          <button class="btn-sm btn-success ml-2" onclick="alert('Hallo ${item.email}');">Nachricht</button>
+        </td>
       </tr>`
   });
   returnHtml += '</table>'
   return returnHtml;
 }
+
 
 module.exports = teacherView;
