ea63f7c0088b56c762e16cd58661b5270ebcd146
Daniel <ds@danielstegen.de>
2020-08-02 15:19:50 +0200

    code cleanup & bug fix

--- views/timetable-view.js
+++ views/timetable-view.js
@@ -8,14 +8,12 @@
 'use strict';
 
 const path = require('path');
-const { thisWeek, weekDates, weekDayNumber, formatDay, formatDate, weekDay, beforeToday, isActualWeek } = require('../lib/dateJuggler');
+const { thisWeek, weekDates, formatDate, weekDay, beforeToday, isActualWeek } = require('../lib/dateJuggler');
 let lessonsConfig = {};
 
 
 function timetableView (myLessons, myGroup, curWeek=thisWeek()) {
   lessonsConfig = require(path.join('../data/classes/', myGroup,'/config.json'));
-  let todayOff = '';
-  if (myLessons.filter( item => item.weekdays.includes(weekDayNumber())).length < 1) todayOff = `<span class="text-muted">- kein Unterricht -</span>`;
   return `
   <div class="container mt-3 p-3">
     <h4>Klasse: ${myGroup}</h4>
@@ -33,48 +31,7 @@ function timetableView (myLessons, myGroup, curWeek=thisWeek()) {
     </div>
     <hr />
     <div class="row text-center">
-      <div class="col-12 col-md-6 col-lg-2 border-right mb-5 mb-lg-0">
-        <h5 class="mb-0 ${beforeToday(1, curWeek)?'text-black-50':''}">${weekDay(1)}</h5>
-        <small class="text-muted">${formatDate(1, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 1, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(1) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
-      <div class="col-12 col-md-6 col-lg-2 border-right mb-5 mb-lg-0">
-        <h5 class="mb-0 ${beforeToday(2, curWeek)?'text-black-50':''}">${weekDay(2)}</h5>
-        <small class="text-muted">${formatDate(2, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 2, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(2) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
-      <div class="col-12 col-md-6 col-lg-2 border-right mb-5 mb-lg-0">
-        <h5 class="mb-0 ${beforeToday(3, curWeek)?'text-black-50':''}">${weekDay(3)}</h5>
-        <small class="text-muted">${formatDate(3, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 3, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(3) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
-      <div class="col-12 col-md-6 col-lg-2 border-right mb-5 mb-lg-0">
-        <h5 class="mb-0 ${beforeToday(4, curWeek)?'text-black-50':''}">${weekDay(4)}</h5>
-        <small class="text-muted">${formatDate(4, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 4, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(4) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
-      <div class="col-12 col-md-6 col-lg-2 border-right mb-5 mb-lg-0">
-        <h5 class="mb-0 ${beforeToday(5, curWeek)?'text-black-50':''}">${weekDay(5)}</h5>
-        <small class="text-muted">${formatDate(5, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 5, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(5) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
-      <div class="col-12 col-md-6 col-lg-2 mb-5 mb-lg-0">
-        <h5 class="mb-0 ${beforeToday(6, curWeek)?'text-black-50':''}">${weekDay(6)}</h5>
-        <small class="text-muted">${formatDate(6, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 6, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(6) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
-      <div class="col-12 mb-5 mb-lg-0 border-top mt-4 pt-3 d-none">
-        <h5 class="mb-0 ${beforeToday(7, curWeek)?'text-black-50':''}">${weekDay(7)}</h5>
-        <small class="text-muted">${formatDate(7, curWeek)}</small>
-        ${myLessons.map(lesson => helperLesson(lesson, 7, curWeek)).join('')}
-        ${myLessons.filter( item => item.weekdays.includes(7) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
-      </div>
+      ${[1,2,3,4,5,6,7].map( day => helperWeekday(day, curWeek, myLessons)).join('')}
     </div>
   </div>
   `;
@@ -82,6 +39,17 @@ function timetableView (myLessons, myGroup, curWeek=thisWeek()) {
 
 
 // Additional functions
+
+function helperWeekday (day, curWeek, myLessons) {
+  return `
+    <div class="col-12 col-md-6 col-lg-2 mb-5 mb-lg-0">
+      <h5 class="mb-0 ${beforeToday(day, curWeek)?'text-black-50':''}">${weekDay(day)}</h5>
+      <small class="text-muted">${formatDate(day, curWeek)}</small>
+      ${myLessons.map(lesson => helperLesson(lesson, day, curWeek)).join('')}
+      ${myLessons.filter( item => item.weekdays.includes(day) && isActualWeek(item.validFrom, item.validUntil, curWeek)).length < 1 ?'<p class="text-muted mt-2">- kein Unterricht -</p>':''}
+    </div>
+  `;
+}
 
 function helperLesson (lessonObj, curDay, curWeek) {
   let lessonColor = '';
