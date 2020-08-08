7728b86615bc498e0cce7870029a0a90444e1f01
Daniel <ds@danielstegen.de>
2020-08-08 22:01:16 +0200

    locale login

--- views/login-view.js
+++ views/login-view.js
@@ -7,6 +7,9 @@
 
 'use strict';
 
+// Required modules
+const locale = require('../lib/locale');
+const config = require('../models/model-config')();
 const getNaviObj = require('./lib/getNaviObj');
 const view = require('./view');
 
@@ -17,20 +20,20 @@ function loginView (wsport, message='') {
     <div class="container d-flex justify-content-center py-5">
       <div class="card loginform-card">
         <div class="card-body">
-          <h2 class="card-title">Bitte anmelden</h2>
+          <h2 class="card-title">${locale.headlines.please_login[config.lang]}</h2>
           <form action="/login" method="post" enctype="application/x-www-form-urlencoded" name="login-form">
             <input type="text" readonly class="d-none" id="action" name="action" value="login">
             <div class="form-group">
-              <label for="login-email">E-Mail-Adresse</label>
-              <input type="email" class="form-control" id="username" name="username" placeholder="E-Mail">
+              <label for="login-email">${locale.login.email_address[config.lang]}</label>
+              <input type="email" class="form-control" id="username" name="username" placeholder="${locale.placeholder.email[config.lang]}">
             </div>
             <div class="form-group">
-              <label for="login-password">Passwort</label>
-              <input type="password" class="form-control" id="password" name="password" placeholder="Passwort">
+              <label for="login-password">${locale.login.password[config.lang]}</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="${locale.placeholder.password[config.lang]}">
             </div>
             <div class=" d-flex justify-content-end">
               <button type="submit" class="btn btn-secondary">
-                Login
+                ${locale.buttons.login[config.lang]}
               </button>
             </div>
           </form>
