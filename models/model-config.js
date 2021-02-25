a4139f5657139d2bdb07a91b77b960df69a34d56
Daniel Stegen <1272638+dstegen@users.noreply.github.com>
2021-02-25 19:33:35 +0100

    bugfix addNewGroup

--- models/model-config.js
+++ models/model-config.js
@@ -35,7 +35,7 @@ function addNewGroup (fields) {
     try {
       myConfig.classes.push(fields.newGroup);
       saveFile(path.join(__dirname, '../data/school'), 'config.json', myConfig)
-      createDir(path.join('data/classes', fields.newGroup));
+      createDir(path.join(__dirname, '../data/classes', fields.newGroup));
       saveFile(path.join(__dirname, '../data/classes', fields.newGroup), 'chat.json', []);
       saveFile(path.join(__dirname, '../data/classes', fields.newGroup), 'config.json', { courses: [] });
       saveFile(path.join(__dirname, '../data/classes', fields.newGroup), 'lessons.json', { lessons: [] });
